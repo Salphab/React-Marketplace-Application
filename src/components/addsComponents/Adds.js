c85cab1c93e01cd857099710aea954cd55f2ab10
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 21:06:04 +0100

    Add files via upload

---
+++ src/components/addsComponents/Adds.js
@@ -0,0 +1,11 @@
+import './Adds.css';
+
+function  Adds(props){
+    return(
+        <div id={props.conid}>
+
+        </div>
+
+    )
+}
+export default Adds;
---
+++ src/components/addsComponents/TopAdd.js
@@ -0,0 +1,13 @@
+import './TopAdd.css';
+function TopAdd (){
+
+    return(
+        <a href='sdfse'>
+            <div  className="topadd">
+                
+            </div>
+        </a>
+        
+    )
+}
+export default TopAdd;
