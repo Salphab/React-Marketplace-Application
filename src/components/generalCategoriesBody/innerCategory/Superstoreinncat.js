69eaca17d2067cddb41bf51ed42cb02c5ef79acf
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 21:35:19 +0100

    Add files via upload

---
+++ src/components/generalCategoriesBody/innerCategory/Superstoreinncat.js
@@ -0,0 +1,11 @@
+import './Superstoreinncat.css';
+
+function Superstoreinncat(){
+    return(
+    <>
+        <div className='Superstoreinn'>
+
+        </div>
+    </>)
+}
+export default Superstoreinncat;
