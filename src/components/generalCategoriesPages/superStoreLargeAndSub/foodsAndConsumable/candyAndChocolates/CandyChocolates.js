dc03ecb1637aeb5dce3b21d185e923d9ad43299c
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 21:49:12 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/candyAndChocolates/CandyChocolates.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/candyAndChocolates/CandyChocolates.js
@@ -1,1 +1,20 @@
-
+const {Candystorecat,Candystorebrands,lightData} = require('../../../../assets/CandyChocolatesAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import CandyChocolatesBody from './CandyChocolatesBody';
+function CandyChocolates(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <CandyChocolatesBody CatData={Candystorecat} BrandsData={Candystorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default CandyChocolates;
