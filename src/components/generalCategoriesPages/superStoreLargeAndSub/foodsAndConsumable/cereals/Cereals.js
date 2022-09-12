62a334aba3e538e409b02f8176efa6676058b131
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 21:54:18 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/cereals/Cereals.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/cereals/Cereals.js
@@ -1,1 +1,20 @@
-
+const {Cerealstorecat,Cerealstorebrands,lightData} = require('../../../../assets/CerealsAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import CerealsBody from './CerealsBody';
+function Cereals(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <CerealsBody CatData={Cerealstorecat} BrandsData={Cerealstorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default Cereals;
