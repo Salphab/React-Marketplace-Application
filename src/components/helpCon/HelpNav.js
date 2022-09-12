e62dee99c71114f869151c8ab1e24577f14d1479
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 22:12:59 +0100

    Add files via upload

--- src/components/helpCon/HelpNav.js
+++ src/components/helpCon/HelpNav.js
@@ -1,1 +1,16 @@
+import './HelpNav.css';
+import {Link} from 'react-router-dom';
 
+function HelpNav(){
+    return(
+    <div className='helpnav'>
+        <ul>
+            <Link to='/'><li>Help Centre</li></Link>
+            <Link to='/'><li>Track Order</li></Link>
+            <Link to='/'><li>Order Cancellation</li></Link>
+            <Link to='/'><li>Return and Refund Policies</li></Link>
+            <Link to='/'><li>Contact Us</li></Link>
+        </ul>
+    </div>)
+}
+export default HelpNav;
