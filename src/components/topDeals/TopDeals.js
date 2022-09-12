71d5ff158dfa67229614b8eddc879c82269be537
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 22:15:37 +0100

    Add files via upload

--- src/components/topDeals/TopDeals.js
+++ src/components/topDeals/TopDeals.js
@@ -1,1 +1,15 @@
 
+import './TopDeals.css';
+
+function TopDeals(props){
+    return(
+        <div id={props.conid}>
+            <header id={props.headerid}>
+                <h2>{props.heading}</h2>
+                <a href='dfh'>{props.more}</a>
+            </header>
+        </div>
+
+    )
+}
+export default TopDeals;
