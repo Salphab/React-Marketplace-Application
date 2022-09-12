f497027253ba8adfae70a22e7ebaff496626ce07
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-12 22:18:31 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/toiletries/Toiletries.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/toiletries/Toiletries.js
@@ -1,1 +1,20 @@
-
+const {Toiletriesstorecat,Toiletriesstorebrands,lightData}= require('../../../assets/ToiletriesAsset');
+import React from 'react';
+import TopAdd from '../../../addsComponents/TopAdd';
+import Header from '../../../headerCon/Header';
+import Footer from '../../../Footers/Footercontainer/Footer';
+import ToiletriesBody from './ToiletreisBody';
+function Toiletries(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <ToiletriesBody CatData={Toiletriesstorecat} BrandsData={Toiletriesstorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default Toiletries;
