ed90a6680674b9abc5466605a999480281ca16a3
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 12:29:35 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/cookingOil/CookingOil.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/cookingOil/CookingOil.js
@@ -1,1 +1,20 @@
-
+const {CookingOilstorecat,CookingOilstorebrands,lightData} = require('../../../../assets/CookingOilAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import CookingOilBody from './CookingOilBody';
+function CookingOil(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <CookingOilBody CatData={CookingOilstorecat} BrandsData={CookingOilstorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default CookingOil;
