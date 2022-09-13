71c727f79431096366459287e9bd258a82119793
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 13:16:52 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/purifiedWater/PurifiedWater.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/purifiedWater/PurifiedWater.js
@@ -1,1 +1,20 @@
-
+const {Waterstorecat,Waterstorebrands,lightData} = require('../../../../assets/PurifiedWaterAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import PuriedWaterBody from './PurifiedWaterBody';
+function PurifiedWater(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <PuriedWaterBody CatData={Waterstorecat} BrandsData={Waterstorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default PurifiedWater;
