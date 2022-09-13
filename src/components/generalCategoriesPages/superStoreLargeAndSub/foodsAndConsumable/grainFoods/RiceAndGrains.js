c873307ee6e831e0cb523973a601aa33fc5db772
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 12:35:34 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/grainFoods/RiceAndGrains.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/grainFoods/RiceAndGrains.js
@@ -1,1 +1,20 @@
-
+const {Noodlestorecat,Noodlestorebrands,lightData} = require('../../../../assets/NoodlesAndPastaAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import RiceAndGrainBody from './RiceAndGrainsBody';
+function RiceAndGrains(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <RiceAndGrainBody CatData={Noodlestorecat} BrandsData={Noodlestorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default RiceAndGrains;
