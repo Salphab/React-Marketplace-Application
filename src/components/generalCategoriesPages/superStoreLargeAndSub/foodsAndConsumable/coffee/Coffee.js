2216dc58c7392342663411dcbdbe815cc0a08ead
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 12:04:47 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/coffee/Coffee.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/coffee/Coffee.js
@@ -1,1 +1,20 @@
-
+const {Coffeestorecat,Coffeestorebrands,lightData}= require('../../../../assets/CoffeeAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import CoffeeBody from './CoffeeBody';
+function Coffee(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <CoffeeBody CatData={Coffeestorecat} BrandsData={Coffeestorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default Coffee;
