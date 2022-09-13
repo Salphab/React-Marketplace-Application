c77b7911d711e29ccc2421144809448e225d5cab
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 12:32:25 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/drinks/Drinks.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/drinks/Drinks.js
@@ -1,1 +1,20 @@
-
+const {Drinkstorecat,Drinkstorebrands,lightData} =require('../../../../assets/DrinksAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import DrinksBody from './DrinksBody';
+function Drinks(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <DrinksBody CatData={Drinkstorecat} BrandsData={Drinkstorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default Drinks;
