e7a720f176e497fd938a2937585bdfb8427c931c
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 13:13:28 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/juice/Juices.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/juice/Juices.js
@@ -1,1 +1,20 @@
-
+const {Juicestorecat,Juicestorebrands,lightData} = require('../../../../assets/JuiceAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import JuicesBody from './JuicesBody';
+function Juices(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <JuicesBody CatData={Juicestorecat} BrandsData={Juicestorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default Juices;
