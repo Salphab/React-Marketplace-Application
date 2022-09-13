295724241bdf9c16f669ee2d3cc4fe4f9da32cf5
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 13:35:52 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/spicesAndSeasoning/SpicesAndSeasoning.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/spicesAndSeasoning/SpicesAndSeasoning.js
@@ -1,1 +1,20 @@
-
+const {Spicestorecat,Spicestorebrands,lightData} = require('../../../../assets/SpicesAndSeasoningAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import SpicesAndSeasoningBody from './SpicesAndSeasoningBody';
+function SpicesAndSeasoning(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <SpicesAndSeasoningBody CatData={Spicestorecat} BrandsData={Spicestorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default SpicesAndSeasoning;
---
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/spicesAndSeasoning/SpicesAndSeasoningBody.js
@@ -0,0 +1,43 @@
+import './SpicesAndSeasoningBody.css';
+import {Link} from 'react-router-dom';
+import Generalstorecat from '../../../../generalCategoriesBody/Subcat/Generalstorecat';
+import Generalstorebrand from '../../../../generalCategoriesBody/Subcat/Generalstorebrand';
+import Generalstoreprice from '../../../../generalCategoriesBody/Subcat/Generalstoreprice';
+import Generalstorerating from '../../../../generalCategoriesBody/Subcat/Generalstorerating';
+import Generalstoreshipping from '../../../../generalCategoriesBody/Subcat/Generalstoreshipping';
+import Superstoreinncat from '../../../../generalCategoriesBody/InnerCategory/Superstoreinncat';
+import Innerbodyitems from '../../../../generalCategoriesBody/Innerbodyitemscontainer/Innerbodyitemscontainer';
+import React from 'react';
+
+function SpicesAndSeasoningBody(props){
+    React.useEffect(()=>{
+        document.title = props.navData.pageHeader;
+     },[])
+    return(
+        <>
+            <div className='navigate'>
+                <div>
+                    <p><Link to='/'>Home</Link></p>
+                    <span>&#62;</span>
+                    <p><Link to={props.navData.routeData[0]}>{props.navData.navigator[0]}</Link></p>
+                    <span>&#62;</span>
+                    <p id='ndlink'><Link to={props.navData.routeData[1]}>{props.navData.navigator[1]}</Link></p>
+                </div>
+            </div>
+            <div className='foodsbodycontainer'>
+                <nav className='aside'>
+                    <Generalstorecat Data={props.CatData}/>
+                    <Generalstorebrand Brands={props.BrandsData}/>
+                    <Generalstoreprice/>
+                    <Generalstorerating/>
+                    <Generalstoreshipping/>
+                </nav>
+                <div className='superstorebody'>
+                    <Superstoreinncat/>
+                    <Innerbodyitems/>
+                </div>
+            </div>
+        </>
+    )
+}
+export default SpicesAndSeasoningBody;
