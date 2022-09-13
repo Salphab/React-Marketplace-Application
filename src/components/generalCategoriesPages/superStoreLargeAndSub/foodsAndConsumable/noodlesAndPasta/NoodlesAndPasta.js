eef13d73aa8ce3b75e28da8715e8f78f6aff1d80
Badmus Ibrahim <85400522+Salphab@users.noreply.github.com>
2022-09-13 13:15:18 +0100

    Add files via upload

--- src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/noodlesAndPasta/NoodlesAndPasta.js
+++ src/components/generalCategoriesPages/superStoreLargeAndSub/foodsAndConsumable/noodlesAndPasta/NoodlesAndPasta.js
@@ -1,1 +1,20 @@
-
+const {Noodlestorecat,Noodlestorebrands,lightData} =require('../../../../assets/NoodlesAndPastaAsset');
+import React from 'react';
+import TopAdd from '../../../../addsComponents/TopAdd';
+import Header from '../../../../headerCon/Header';
+import Footer from '../../../../Footers/Footercontainer/Footer';
+import NoodlesAndPastaBody from './NoodlesAndPastaBody';
+function NoodlesAndPasta(){
+  React.useEffect(()=>{
+     
+  },[])
+    return(
+        <>
+            <TopAdd/>
+            <Header/>
+            <NoodlesAndPastaBody CatData={Noodlestorecat} BrandsData={Noodlestorebrands} navData={lightData}/>
+            <Footer/>
+        </>
+    )
+}
+export default NoodlesAndPasta;
